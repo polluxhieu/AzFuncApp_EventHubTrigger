8e6183fff0966c287fe419e24c186d32e00c88fb
Hieu Tran <hieut@qssolutions.nl>
2018-10-18 15:47:23 +0700

    Add project files.

---
+++ TriggersInOut/EventHubTrigger.cs
@@ -0,0 +1,31 @@
+using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace TriggersInOut
+{
+    public static class EventHubTrigger
+    {
+        [FunctionName("EventHubDemoTrigger")]
+        public static void Run(
+            [EventHubTrigger("group3-demo-eh", Connection = "EventHubConnection")]EventData data, ILogger log,
+            [CosmosDB("group3-triggers-inoutbindings", "eventshub", ConnectionStringSetting = "StorageConnection")] IAsyncCollector<EventResult> result)
+        {
+            log.LogInformation($"Event Hub Demo Trigger processed a message: {data}");
+
+            // Metadata accessed by binding to EventData
+            log.LogInformation($"EnqueuedTimeUtc={data.SystemProperties.EnqueuedTimeUtc}");
+            log.LogInformation($"SequenceNumber={data.SystemProperties.SequenceNumber}");
+            log.LogInformation($"Offset={data.SystemProperties.Offset}");
+
+            var eventResult = new EventResult
+            {
+                EnqueuedTimeUtc = data.SystemProperties.EnqueuedTimeUtc,
+                SequenceNumber = data.SystemProperties.SequenceNumber,
+                Offset = data.SystemProperties.Offset
+            };
+            result.AddAsync(eventResult);
+        }
+    }
+}
---
+++ TriggersInOut/EventResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TriggersInOut
+{
+    public class EventResult
+    {
+        public DateTime EnqueuedTimeUtc { get; set; }
+        public long SequenceNumber { get; set; }
+        public string Offset { get; set; }
+    }
+}
